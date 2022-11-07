bf811904d3e4a03b5b88b5fc2fac963f6593732b
Chetan Mahale <97821311+chetan0220@users.noreply.github.com>
2022-11-07 15:01:23 +0530

    Update pinnacle.c

--- pinnacle.c
+++ pinnacle.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct node 
+struct node
 {
     char name[10];
     int prn ;
@@ -20,7 +20,22 @@ void insertSecretary(struct node * head)
     new->prev = NULL;
     head->next = new;
     new->prev = head;
-    
+   
+}
+void insertMember(struct node *head)
+{
+    struct node *new;
+    new=(struct node *)malloc(sizeof(struct node));
+    printf("enter member name:\n");
+    scanf(" %s",new->name);
+    printf("enter member's prn\n");
+    scanf("%d",&new->prn);
+   
+    new->next=NULL;
+    new->prev=NULL;
+    new->next=head->next;
+    head->next=head;
+    new->next->prev=new;
 }
 void display(struct node * head)
 {
@@ -34,6 +49,14 @@ void display(struct node * head)
         temp=temp->next;
     }
 }
+void deleteMember(struct node * head)
+{
+    
+    
+    
+    
+    
+}
 int main()
 {
     struct node * head ;
@@ -45,77 +68,28 @@ int main()
     head->next = NULL;
     head->prev = NULL;
     insertSecretary(head);
-    display(head);
     int ch , y ;
     do
     {
         printf("1.insert member\n2.delete member\n3.delete president\n4.delete secretary\n5.count\n6.reverse\n7.display\n");
+        printf("enter choice : ");
         scanf("%d",&ch);
         switch(ch)
         {
             case 1 :
-            insertMember(struct node * head);
-            
-            
+            insertMember(head);
             break;
             
-            case 2 :
-            deleteMember(struct node * head);
-            
-            
+            case 2 : 
+            display(head);
             break;
             
-            case 3 :
-            deletePresident(struct node * head);
-            
-            
-            
-            
-            break;
-            
-            case 4 :
-            
-            deleteSecretary(struct node * head);
-            
-            
-            
-            
-            
-            break;
-            
-            case 5 :
-            count(struct node * head);
-            
-            
-            
-            
-            
-            break;
-            
-            case 6 :
-            
-            reverse(struct node * head);
-            
-            
-            break;
-            
-            case 7 :
-            
-            
-            display(struct node * head);
-            
-            
-            
-            
-            break;
+            case 3 : 
+            deleteMember(head);
             default :
             printf("enter valid choice ");
         }
-        
-    }while(ch<=7 && ch >=1);
-    
-
-
-
+       
+    }while(ch<=2 && ch >=1);
     return 0;
 }
